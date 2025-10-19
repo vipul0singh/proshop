b93e1369bc61994122ba0bb429b5fecaba30feab
vipul0singh <108611046+vipul0singh@users.noreply.github.com>
2025-10-19 16:39:47 +0530

    middleware,errorHandlers,Routes

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,24 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
+import { notFound,errorHandler } from './middleware/errorMiddleware.js';
 connectDB();
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
 
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
+
+
+app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
