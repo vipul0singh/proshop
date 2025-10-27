196e592cb4545618a650d00be072848b8ba3d23d
vipul0singh <108611046+vipul0singh@users.noreply.github.com>
2025-10-27 14:26:58 +0530

    build fixes-2

--- backend/server.js
+++ backend/server.js
@@ -41,9 +41,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use('/uploads', express.static('/var/data/uploads'));
   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-  app.get('/*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
+ app.all('(.*)', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+});
+
 } else {
   const __dirname = path.resolve();
   app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
